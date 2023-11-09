31a09bf06d2653f924fedc5cd35169cadc2c0a79
7erp1c <ul_tray@bk.ru>
2023-11-09 08:21:10 +0300

    hot_dog

---
+++ 8kuy/holiday_VIII_duty_free.js
@@ -0,0 +1,7 @@
+//The purpose of this kata is to work out just how many bottles of duty free whiskey you would have to buy such that the savings
+// over the normal high street price would effectively cover the cost of your holiday.
+//All inputs will be integers. Please return an integer. Round down.
+
+function dutyFree(normPrice, discount, hol){
+    return Math.floor(hol/((normPrice*discount)/100))
+}
