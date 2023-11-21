48de5d3612477534dcf054f1cf07fee5f9b51c72
7erp1c <ul_tray@bk.ru>
2023-11-21 18:02:33 +0300

    Convert_a_Boolean_to_a_String.js

---
+++ 8kuy/Convert_a_Boolean_to_a_String.js
@@ -0,0 +1,11 @@
+//Реализовать функцию booleanToString(b), которая принимает значение b и
+// возвращает строковое представление значение b в формате "true" или "false".
+
+
+function booleanToString(b){
+    if (b){
+        return 'true';
+    } else {
+        return 'false';
+    }//your code here
+}
