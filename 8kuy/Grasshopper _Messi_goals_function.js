a90b936338b6d7a61881ef791d56cf7bdbbbfca7
7erp1c <ul_tray@bk.ru>
2023-11-21 18:33:05 +0300

    Messi_goal

---
+++ 8kuy/Grasshopper _Messi_goals_function.js
@@ -0,0 +1,4 @@
+// Мессии забивает голы в трёх лигах, написать функцию которая посчитает его голы во всех лигах;
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    return  laLigaGoals+copaDelReyGoals+championsLeagueGoals;
+}
