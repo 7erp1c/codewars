e5baf5976a865842325d50d4cf2075dc0ebcf1d3
7erp1c <ul_tray@bk.ru>
2023-12-01 18:35:50 +0300

    конвектируем массив - на +; + на -.js

---
+++ 7kuy/Vowel Count.js
@@ -0,0 +1,13 @@
+// получаем строку; надо посчитать скольк в ней гласных ===  a, e, i, o, u
+function getCount(str) {
+    let res = 0;
+
+    let arr = str.split('');//
+    console.log(arr)
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 'a' || arr[i] === 'e' || arr[i] === 'i' || arr[i] === 'o' || arr[i] === 'u')
+            res++;
+    }
+    return res;
+}
+console.log(getCount('aefgrgij'))
