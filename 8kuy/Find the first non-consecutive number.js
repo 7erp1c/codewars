c204956b573bc2b74cf31d98119910a8da754ae8
7erp1c <ul_tray@bk.ru>
2023-12-05 16:10:50 +0300

    find();Number.isInteger; null.js

--- 8kuy/Find the first non-consecutive number.js
+++ 8kuy/Find the first non-consecutive number.js
@@ -2,5 +2,5 @@
 function firstNonConsecutive (arr) {
     let arra = arr.find((a,b)=>a !== b + arr[0]);
     return Number.isInteger(arra)?arra:null;
-}
+}//
 console.log(firstNonConsecutive([6,7,8,10,11,12]));
