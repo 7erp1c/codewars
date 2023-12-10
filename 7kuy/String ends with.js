7a9e6b8b439b624b8dae286e8423008d7753a77d
7erp1c <ul_tray@bk.ru>
2023-12-10 20:08:49 +0300

    .endsWith.js

---
+++ 7kuy/String ends with.js
@@ -0,0 +1,17 @@
+function solution(str, ending){
+    return str.endsWith(ending)
+}
+console.log(solution('abc', 'bc'))
+
+
+function solution(str, ending){
+   // console.log(typeof(ending));
+    let regexp = new RegExp(`${ending}`,'gi') //
+    let  regexes = str.match(regexp);
+    //console.log(regexes);
+    let obj = regexes.hasOwnProperty(regexes[0]);
+    console.log(obj);
+    return ending != regexes[0]? false:true;
+}
+console.log(solution('abc', 'ab'));
+//console.log(solution('abc', 'd'));
