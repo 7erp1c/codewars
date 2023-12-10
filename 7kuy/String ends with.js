c18acc03be483faf1edfcd154151f91cb2b7cb72
7erp1c <ul_tray@bk.ru>
2023-12-10 20:16:07 +0300

    .endsWith.js

--- 7kuy/String ends with.js
+++ 7kuy/String ends with.js
@@ -4,14 +4,14 @@ function solution(str, ending){
 console.log(solution('abc', 'bc'))
 
 
-function solution(str, ending){
-   // console.log(typeof(ending));
-    let regexp = new RegExp(`${ending}`,'gi') //
-    let  regexes = str.match(regexp);
-    //console.log(regexes);
-    let obj = regexes.hasOwnProperty(regexes[0]);
-    console.log(obj);
-    return ending != regexes[0]? false:true;
-}
-console.log(solution('abc', 'ab'));
-//console.log(solution('abc', 'd'));
+// function solution(str, ending){
+//    // console.log(typeof(ending));
+//     let regexp = new RegExp(`${ending}`,'gi') //
+//     let  regexes = str.match(regexp);
+//     //console.log(regexes);
+//     let obj = str.regexes;
+//     console.log(obj);
+//     return ending != regexes[0]? false:true;
+// }
+// console.log(solution('abc', 'ab'));
+// //console.log(solution('abc', 'd'));
