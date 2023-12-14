03943876072c547f9d02b589f840c3708d35ecc3
7erp1c <ul_tray@bk.ru>
2023-12-14 20:42:13 +0300

    Найти промежуточное число, и передать его индекс.

---
+++ 7kuy/Find the middle element.js
@@ -0,0 +1,7 @@
+// Найти промежуточное число, и передать его индекс.
+function gimme (triplet) {
+return triplet.indexOf([...triplet].sort((a,b) => a-b)[1]);
+}
+
+console.log(gimme([2, 3, 1]));
+console.log(gimme([5, 10, 14]));
