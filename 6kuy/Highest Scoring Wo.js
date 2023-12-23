882db41e0cafd9645d5cb4d2f2475ce494806d2f
7erp1c <ul_tray@bk.ru>
2023-12-23 17:25:19 +0300

    alphabet.split

---
+++ 6kuy/Highest Scoring Wo.js
@@ -0,0 +1,6 @@
+function high(x){
+    let alphabet = ['0abcdefghijklmnopqrstuvwxyz'];
+    let alp = alphabet.split(' ')
+    console.log(alphabet);
+}
+console.log(high('man i need a taxi up to ubud'));
